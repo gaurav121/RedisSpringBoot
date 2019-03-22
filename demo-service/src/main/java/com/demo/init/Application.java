b57ca0bf8c90c8e1705b0e03b876f926fbefbaac
Gaurav Yadav <gaurav5.yadav@paytm.com>
2019-03-22 22:46:52 +0530

    temp commit

--- demo-service/src/main/java/com/demo/init/Application.java
+++ demo-service/src/main/java/com/demo/init/Application.java
@@ -24,6 +24,7 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
